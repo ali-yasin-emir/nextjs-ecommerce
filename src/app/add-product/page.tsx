513b048ec796bde07f05adb36cd2ca747ce7b130
Ali Yasin Emir <aliyasin.emir16@hotmail.com>
2023-12-21 16:19:44 +0300

    I made the server actions and db operations related with page

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,25 +1,79 @@
-
+import { prisma } from "@/lib/db/prisma";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: "Add Product - Flowmazon"
+  title: "Add Product - Flowmazon",
 };
+
+async function addProduct(formData: FormData) {
+  "use server";
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw new Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: {
+      name,
+      description,
+      imageUrl,
+      price,
+    },
+  });
+
+  redirect("/");
+}
 
 const AddProductPage = () => {
   return (
-<div className="bg-gray-300 h-screen flex justify-center">
-    <div className="w-full max-container flex flex-col">
-      <form className="flex flex-col border-2 bg-gradient-to-r from-pink-500 to-purple-600  my-24 p-12 border-red-300 gap-6 rounded-lg">
-      <h1 className="text-2xl font-bold text-white">Add Product Page</h1>
-      <input required type="text" placeholder="Name" className="text-black py-4 input input-lg input-bordered w-full"  />
-      <textarea required name="description" placeholder="Description" rows={8} cols={12} className="py-4 w-full textarea textarea-lg textarea-bordered resize-none"/>
-      <input required type="url" placeholder="Image URL" className="text-black py-4 input input-bordered input-lg"/>
-      <input required min={1} type="number" placeholder='Price' className="text-black py-4 input input-bordered input-lg"/>
-      <button type="submit" className="text-lg btn btn-primary btn-block">Add Product</button>
-      </form>
+    <div className="flex h-screen justify-center bg-gray-300">
+      <div className="max-container flex w-full flex-col">
+        <form
+          action={addProduct}
+          className="my-24 flex flex-col gap-6 rounded-lg border-2  border-red-300 bg-gradient-to-r from-pink-500 to-purple-600 p-12"
+        >
+          <h1 className="text-2xl font-bold text-white">Add Product Page</h1>
+          <input
+            required
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="input input-bordered input-lg w-full py-4 text-black"
+          />
+          <textarea
+            required
+            name="description"
+            placeholder="Description"
+            rows={8}
+            cols={12}
+            className="textarea textarea-bordered textarea-lg w-full resize-none py-4"
+          />
+          <input
+            required
+            type="url"
+            name="imageUrl"
+            placeholder="Image URL"
+            className="input input-bordered input-lg py-4 text-black"
+          />
+          <input
+            required
+            min={1}
+            type="number"
+            name="price"
+            placeholder="Price"
+            className="input input-bordered input-lg py-4 text-black"
+          />
+          <button type="submit" className="btn btn-primary btn-block text-lg">
+            Add Product
+          </button>
+        </form>
       </div>
     </div>
-  )
-}
-export default AddProductPage
-
+  );
+};
+export default AddProductPage;
